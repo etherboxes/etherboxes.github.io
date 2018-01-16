f1fa4c571bd1d155f611a23d4798384699409d7c
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2018-01-15 19:26:25 -0500

    fix that

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <h1 className="page-header">EtherSquares</h1>
-        <p>Welcome to EtherBoxes, where you can play the boxes on the Big Game with Ether.</p>
+        <p>Welcome to EtherSquares, where you can play the boxes on the Big Game with Ether.</p>
 
         <h2>How it works</h2>
         <p>
-          EtherBoxes allows you play as many boxes as you want by allowing you buy a stake in a box. Simply pick a box
-          and enter the amount of Ether you want to wager. At the end of the Big Game, come back to EtherBoxes and
+          EtherSquares allows you play as many boxes as you want by allowing you buy a stake in a box. Simply pick a box
+          and enter the amount of Ether you want to wager. At the end of the Big Game, come back to EtherSquares and
           collect your portion of the winnings. There is a payout for each quarter and your portion of the winnings for
           a quarter is equal to a fourth of the total pot multiplied by your percentage stake in the winning the box.
           You can wager right up until the Big Game starts on February 4 at 6:30 PM EST.
@@ -24,7 +24,7 @@ export default class App extends React.Component {
         <br/>
 
         <p>
-          The table above is last year's box score for the Big Game. EtherBoxes would've paid out for the first three
+          The table above is last year's box score for the Big Game. EtherSquares would've paid out for the first three
           quarters and the final score -- we don't include overtime as a quarter.
         </p>
 
@@ -46,8 +46,8 @@ export default class App extends React.Component {
 
         <h2>About us</h2>
         <p>
-          We here at EtherBoxes are committed to a trustworthy and open experience.
-          Feel free to audit all of our code <a href="https://github.com/etherboxes">here</a>.
+          We here at EtherSquares are committed to a trustworthy and open experience.
+          Feel free to audit all of our code <a href="https://github.com/moodysalem/ethersquares">here</a>.
         </p>
         <p>Our service charges 5% of each wager which gets transfered when the wager is made.</p>
       </div>
